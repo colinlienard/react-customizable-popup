49d0dea38bbbe7c76290214c66139df22f43e850
Colin Lienard <colin.lienard87@orange.fr>
2021-12-14 22:38:32 +0100

    chore(storybook): update to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
 }
