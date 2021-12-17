d37140d3e6059ef0abd6c7413cd0d08b94c6e9e7
Colin Lienard <colin.lienard87@orange.fr>
2021-12-17 15:10:49 +0100

    feat(popup): click or hover to open the popup

--- src/index.tsx
+++ src/index.tsx
@@ -21,12 +21,14 @@ export type Props = {
     'center' | 'left' | 'midleft' | 'right' | 'midright',
     'center' | 'top' | 'midtop' | 'bottom' | 'midbottom',
   ],
+  toggleOn?: 'click' | 'hover',
 }
 
 const Popup: FC<Props> = ({
   children,
   toggler,
-  position,
+  position = ['center', 'bottom'],
+  toggleOn = 'click',
 }) => {
   const [open, setOpen] = useState<boolean>(false);
   const [pos, setPos] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
@@ -39,75 +41,74 @@ const Popup: FC<Props> = ({
 
   const getPosition = () => {
     if (popupRef.current && togglerRef.current) {
-      let x = togglerRef.current.offsetLeft
-        + togglerRef.current.offsetWidth / 2
-        - popupRef.current.offsetWidth / 2;
+      let x = 0;
+      let y = 0;
 
-      let y = togglerRef.current.offsetTop
-        + togglerRef.current.offsetHeight
-        + distanceFromToggler;
-
-      if (position) {
-        switch (position[0]) {
-          case 'center': {
-            break;
-          }
-          case 'left': {
-            x = togglerRef.current.offsetLeft
-              - popupRef.current.offsetWidth
-              - distanceFromToggler;
-            break;
-          }
-          case 'midleft': {
-            x = togglerRef.current.offsetLeft
-              + togglerRef.current.offsetWidth
-              - popupRef.current.offsetWidth;
-            break;
-          }
-          case 'right': {
-            x = togglerRef.current.offsetLeft
-              + togglerRef.current.offsetWidth
-              + distanceFromToggler;
-            break;
-          }
-          case 'midright': {
-            x = togglerRef.current.offsetLeft;
-            break;
-          }
-          default: {
-            throw new Error('Position on the horizontal axis : wrong value provided.');
-          }
+      switch (position[0]) {
+        case 'center': {
+          x = togglerRef.current.offsetLeft
+            + togglerRef.current.offsetWidth / 2
+            - popupRef.current.offsetWidth / 2;
+          break;
         }
+        case 'left': {
+          x = togglerRef.current.offsetLeft
+            - popupRef.current.offsetWidth
+            - distanceFromToggler;
+          break;
+        }
+        case 'midleft': {
+          x = togglerRef.current.offsetLeft
+            + togglerRef.current.offsetWidth
+            - popupRef.current.offsetWidth;
+          break;
+        }
+        case 'right': {
+          x = togglerRef.current.offsetLeft
+            + togglerRef.current.offsetWidth
+            + distanceFromToggler;
+          break;
+        }
+        case 'midright': {
+          x = togglerRef.current.offsetLeft;
+          break;
+        }
+        default: {
+          throw new Error('Position on the horizontal axis : wrong value provided.');
+        }
+      }
 
-        switch (position[1]) {
-          case 'center': {
-            y = togglerRef.current.offsetTop
-              + togglerRef.current.offsetHeight / 2
-              - popupRef.current.offsetHeight / 2;
-            break;
-          }
-          case 'top': {
-            y = togglerRef.current.offsetTop
-              - popupRef.current.offsetHeight
-              - distanceFromToggler;
-            break;
-          }
-          case 'midtop': {
-            y = togglerRef.current.offsetTop
-              + togglerRef.current.offsetHeight
-              - popupRef.current.offsetHeight;
-            break;
-          }
-          case 'bottom': {
-            break;
-          }
-          case 'midbottom': {
-            y = togglerRef.current.offsetTop;
-            break;
-          }
-          default: {
-            throw new Error('Position on the vertical axis : wrong value provided.');
-          }
+      switch (position[1]) {
+        case 'center': {
+          y = togglerRef.current.offsetTop
+            + togglerRef.current.offsetHeight / 2
+            - popupRef.current.offsetHeight / 2;
+          break;
+        }
+        case 'top': {
+          y = togglerRef.current.offsetTop
+            - popupRef.current.offsetHeight
+            - distanceFromToggler;
+          break;
+        }
+        case 'midtop': {
+          y = togglerRef.current.offsetTop
+            + togglerRef.current.offsetHeight
+            - popupRef.current.offsetHeight;
+          break;
+        }
+        case 'bottom': {
+          y = togglerRef.current.offsetTop
+            + togglerRef.current.offsetHeight
+            + distanceFromToggler;
+          break;
+        }
+        case 'midbottom': {
+          y = togglerRef.current.offsetTop;
+          break;
+        }
+        default: {
+          throw new Error('Position on the vertical axis : wrong value provided.');
         }
       }
 
@@ -139,14 +140,27 @@ const Popup: FC<Props> = ({
     setRoot(newRoot);
   };
 
+  const openPopup = () => setOpen(true);
+
+  const closePopup = () => setOpen(false);
+
   const togglePopup = () => setOpen((state) => !state);
 
   return (
     <>
-      {cloneElement(toggler, {
-        onClick: togglePopup,
-        ref: togglerRef,
-      })}
+      {cloneElement(
+        toggler,
+        toggleOn === 'click'
+          ? {
+            onClick: togglePopup,
+            ref: togglerRef,
+          }
+          : {
+            onMouseEnter: openPopup,
+            onMouseLeave: closePopup,
+            ref: togglerRef,
+          },
+      )}
       {createPortal(
         <>
           <div
@@ -159,12 +173,14 @@ const Popup: FC<Props> = ({
           >
             {children}
           </div>
-          <div
-            className={`cpopup-background default ${open && 'active'}`}
-            onClick={togglePopup}
-            role="button"
-            aria-hidden="true"
-          />
+          {toggleOn === 'click' && (
+            <div
+              className={`cpopup-background default ${open && 'active'}`}
+              onClick={togglePopup}
+              role="button"
+              aria-hidden="true"
+            />
+          )}
         </>,
         document.querySelector(root) as Element,
       )}
