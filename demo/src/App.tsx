e0cbda576c7f59be81507f9972820b3409bffeda
Colin Lienard <colin.lienard87@orange.fr>
2021-12-24 16:01:24 +0100

    feat(demo): demo completed

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable import/no-absolute-path */
 /* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
@@ -58,6 +59,91 @@ const App = () => {
           >
             <p>It seems to work.</p>
           </Popup>
+        </section>
+        <section>
+          <h2>Different positions</h2>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            position={['right', 'center']}
+          >
+            <p>The value is <span className="link">[&apos;right&apos;, &apos;center&apos;]</span>.</p>
+          </Popup>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            position={['center', 'top']}
+          >
+            <p>The value is <span className="link">[&apos;center&apos;, &apos;top&apos;]</span>.</p>
+          </Popup>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            position={['midleft', 'bottom']}
+          >
+            <p>The value is <span className="link">[&apos;midleft&apos;, &apos;bottom&apos;]</span>.</p>
+          </Popup>
+        </section>
+        <section>
+          <h2>Scroll allowed</h2>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            noScroll={false}
+          >
+            <p>You can scroll even if the popup is open.</p>
+          </Popup>
+        </section>
+        <section>
+          <h2>Fixed on scroll</h2>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            fixed
+          >
+            <p>This popup is fixed.</p>
+          </Popup>
+        </section>
+        <section>
+          <h2>No background</h2>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            background={false}
+          >
+            <button className="cross" type="button" data-close>
+              <img src={cross} alt="" />
+            </button>
+            <p>This popup has no background.</p>
+          </Popup>
+        </section>
+        <section>
+          <h2>Styled popup</h2>
+          <Popup
+            toggler={
+              <button className="button" type="button">Click me</button>
+            }
+            className="custom-popup"
+            backgroundClassName="custom-background-popup"
+            arrowSize={20}
+            position={['center', 'top']}
+            distanceFromToggler={40}
+          >
+            <button className="cross" type="button" data-close>
+              <img src={cross} alt="" />
+            </button>
+            <p>This popup is styled !</p>
+          </Popup>
+        </section>
+        <section>
+          <h2>And many other options...</h2>
+          <a className="link" href="https://github.com/ColinLienard/react-customizable-popup#readme" target="_blank" rel="noreferrer">Documentation</a>
         </section>
       </main>
     </>
